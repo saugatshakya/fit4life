a4eb14bfedd578abc84add0eafe0e169cd7547bd
unknown <minugrg55@gmail.com>
2020-05-26 15:24:35 +0545

    changes made

--- lib/models/nutrition.dart
+++ lib/models/nutrition.dart
@@ -1,5 +1,6 @@
 class Nutrition {
-  final String name, desc, breakfast, lunch, dinner, breaKfastImage, lunchImage, dinnerImage;
+  final String name, desc, info, breaKfastImage, lunchImage, dinnerImage;
+  final List<String> descr, breakfast, lunch, dinner;
   final int breakfastCal, lunchCal, dinnerCal;
   Nutrition({this.name,this.desc,this.breakfast,this.lunch,this.dinner,this.breakfastCal,this.dinnerCal,this.lunchCal,this.breaKfastImage,this.lunchImage,this.dinnerImage});
 }
